866e4aabb9e312f6c9abe5a2ffd39b996ba1095e
Abhilash Jha <abhilashjha121@gmail.com>
2026-04-14 10:53:19 +0530

    Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
@@ -9,7 +9,7 @@ import {Toaster} from 'react-hot-toast';
 export default function App() {
   const token = localStorage.getItem("jwt");
   return (
-   <BrowserRouter>
+   <div>
     <Routes>
       <Route path="/" element={token ? <Home/> : <Navigate to={"/login"}/>}/>
       <Route path="/login" element={<Login/>}/>
@@ -17,6 +17,6 @@ export default function App() {
       <Route path="*" element={<Pagenotfound/>}/>
     </Routes>
     <Toaster/>
-   </BrowserRouter>
+   </div>
   )
 }
